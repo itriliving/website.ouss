2f383dbed9c2f16c4da851c591e894b424833729
Oussama Hammach <49763083+OussamaHm@users.noreply.github.com>
2024-09-16 01:38:01 +0100

    Update exceptional.js

--- components/exceptional.js
+++ components/exceptional.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./exceptional.module.css";
 
 const Exceptional = ({ className = "" }) => {
@@ -10,39 +10,20 @@ const Exceptional = ({ className = "" }) => {
     b4: "",
     b5: ""
   });
-  const [isVisible, setIsVisible] = useState(false); 
-  const sectionRef = useRef(null); 
+    
   useEffect(() => {
-    const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL; 
+    const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL; // Fetch the base URL from env variables
     fetch(`${baseURL}/get_data3`)
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
-  
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const entry = entries[0];
-        if (entry.isIntersecting) {
-          setIsVisible(true); 
-          observer.disconnect(); 
-        }
-      },
-      { threshold: 0.1 } 
-    );
-  
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current); 
-    }
-  
-    return () => observer.disconnect(); 
-  }, []);
+
   return (
     <section className={[styles.exceptional, className].join(" ")}>
       <div className={styles.exceptionalContentParent}>
         <div className={styles.exceptionalContent}>
-          <h1 className={`${styles.mediumLengthHeroContainer}  ${!isVisible ? styles.hidden :  styles.slideInFromRight}`}>
+          <h1 className={styles.mediumLengthHeroContainer}>
             <span
               className={styles.benefitsToBe}
             >{data.b1}</span>
@@ -55,11 +36,11 @@ const Exceptional = ({ className = "" }) => {
                       href="https://calendly.com/admin-itri/itri-living-concept-explained" 
                           target="_blank" 
                           rel="noopener noreferrer"
-                           className={styles.noUnderline} 
+                           className={styles.noUnderline} // Ensure there's no underline
                                 >
-                                     Express your interest
-                                          </a>
-                                   </div>
+    Express your interest
+  </a>
+</div>
 
             </div>
           </div>
