025a87e6789b64c5f09b135929d1cf379f0cb400
Oussama Hammach <49763083+OussamaHm@users.noreply.github.com>
2024-09-16 01:30:11 +0100

    Update exceptional.js

--- components/exceptional.js
+++ components/exceptional.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styles from "./exceptional.module.css";
 
 const Exceptional = ({ className = "" }) => {
