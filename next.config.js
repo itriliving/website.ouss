feaa2519ee46128e3177107e29a5518c83633358
Oussama Hammach <49763083+OussamaHm@users.noreply.github.com>
2024-10-25 15:47:10 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
 /** @type {import('next').NextConfig} */
 
-  const nextConfig = {
-    reactStrictMode: true,
+const nextConfig = {
+  reactStrictMode: true,
   images: {
-
     domains: ['flagcdn.com'],
-    
   },
+  async headers() {
+    return [
+      {
+        // Configure les en-têtes pour les fichiers JSON de traduction
+        source: '/locales/:locale/:file*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', // Mise en cache pendant 1 an
+          },
+        ],
+      },
+    ];
+  },
+};
 
-  }
-
-  module.exports = nextConfig
+module.exports = nextConfig;
